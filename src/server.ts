f77e96c00c02e10969ec2f5d8a832de1b44ef49e
Jason Aravanis <j.aravanis@icloud.com>
2021-12-13 10:05:52 +1100

    Add debug message setup

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import app from "./app/app";
 import mongoConfig from "./mongoConfig";
+const debug = require("debug")("facebook:server");
 require("dotenv").config();
 
 mongoConfig();
-app.listen(process.env.PORT, () => `Listening on port ${process.env.PORT}`);
+app.listen(process.env.PORT, () =>
+  debug(`Listening on port ${process.env.PORT}`)
+);
