c5d1d726bff4234a4720cec26f0a53eb0ebdd80c
Jason Aravanis <j.aravanis@icloud.com>
2022-01-17 16:25:47 +1100

    Add todo note about API security on posts endpoints

--- src/routes/posts/posts.ts
+++ src/routes/posts/posts.ts
@@ -5,6 +5,10 @@ import {
   getPost,
 } from "../../controllers/posts/posts";
 
+// TODO: Clarify if need to add "ensureAuthenticated" to these routes. As these will be accessed via front end, may be able to use CORS
+// to prevent unauthorized access (i.e one user requesting the newsfeed of a different user). For future reference, the way to see if a
+// user session exists (on the server) is at req.session.passport.user
+
 const router = Router();
 
 router.get("/newsfeed/:uid", getNewsfeedPosts);
