eb1d9dd9b52626891edb7602794caf23a15d44be
Jason Aravanis <j.aravanis@icloud.com>
2022-01-19 12:08:13 +1100

    Add mock comments to populateMockDatabase

--- src/utils/populateMockDatabase.ts
+++ src/utils/populateMockDatabase.ts
@@ -1,10 +1,15 @@
 import User, { UserInterface } from "../models/User";
 import Post, { PostInterface } from "../models/Post";
+import Comment, { CommentInterface } from "../models/Comment";
+import { HydratedDocument } from "mongoose";
+const debug = require("debug")("facebook:utils/populateMockDatabase");
 
 // This function populates mongodb-memory-server for testing.
 // Note: Database should be dropped in a beforeEach() in test file before calling this function.
 
 export default async function populateMockDatabase() {
+  // ***** USERS *****
+
   const mockUsers: UserInterface[] = [
     {
       email: "steve@rogers.com",
@@ -32,6 +37,16 @@ export default async function populateMockDatabase() {
 
   const users = await User.find({});
   const mockUserIds: string[] = users.map((objId) => objId.id);
+
+  // make steve and tony friends
+  const steve = await User.findById(mockUserIds[0]);
+  const tony = await User.findById(mockUserIds[1]);
+  steve?.friends?.push(mockUserIds[1]);
+  tony?.friends?.push(mockUserIds[0]);
+  steve?.save();
+  tony?.save();
+
+  // ***** POSTS *****
 
   const mockPosts: PostInterface[] = [
     {
@@ -61,13 +76,34 @@ export default async function populateMockDatabase() {
   const posts = await Post.find({});
   const mockPostIds: string[] = posts.map((objId) => objId.id);
 
-  // make steve and tony friends
-  const steve = await User.findById(mockUserIds[0]);
-  const tony = await User.findById(mockUserIds[1]);
-  steve?.friends?.push(mockUserIds[1]);
-  tony?.friends?.push(mockUserIds[0]);
-  steve?.save();
-  tony?.save();
+  // ***** COMMENTS *****
 
-  return { mockUserIds, mockPostIds };
+  const mockComments: HydratedDocument<CommentInterface>[] = [
+    new Comment({
+      author: mockUserIds[0],
+      content: "1st comment - author[0] - post[0]",
+      postID: mockPostIds[0],
+    }),
+    new Comment({
+      author: mockUserIds[1],
+      content: "2nd comment - author[1] - post[0]",
+      postID: mockPostIds[0],
+    }),
+    new Comment({
+      author: mockUserIds[2],
+      content: "3rd comment - author[2] - post[0]",
+      postID: mockPostIds[0],
+    }),
+  ];
+
+  const mockCommentIds = [];
+
+  for (const comment of mockComments) {
+    mockCommentIds.push(comment.id);
+    let post = await Post.findById(comment.postID);
+    post?.comments.push(comment);
+    await post?.save();
+  }
+
+  return { mockUserIds, mockPostIds, mockCommentIds };
 }
