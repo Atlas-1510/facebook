aead59c265f47f2d2f74e3c87b67ec3136599cab
Jason Aravanis <j.aravanis@icloud.com>
2022-01-23 09:04:22 +1100

    Add comment to index.d.ts

--- src/typings/index.d.ts
+++ src/typings/index.d.ts
@@ -2,7 +2,7 @@ export {};
 declare global {
   namespace jest {
     interface Matchers<R> {
-      toBeDistinct(): R;
+      toBeDistinct(): R; // This is used to add the custom jest matcher .toBeDistinct() to typescript
     }
   }
 }
