554c3957f7a576619c7b1f456dc944df87dc3279
Jason Aravanis <j.aravanis@icloud.com>
2022-01-24 16:49:09 +1100

    Setup tailwind JIT (v3.0 configuration)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <div className="text-red-500">Hello World</div>;
+  return <div className="text-red-500 bg-[#1da1f1]">Hello World</div>;
 }
 
 export default App;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  purge: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
+    "./src/**/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
