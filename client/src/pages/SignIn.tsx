84ab3656a78a01e629a7f15e6df176efe15f93ba
Jason Aravanis <j.aravanis@icloud.com>
2022-01-25 17:16:59 +1100

    Add mobile design of signin page

--- client/src/pages/SignIn.tsx
+++ client/src/pages/SignIn.tsx
@@ -2,8 +2,8 @@ import { FC } from "react";
 
 const SignIn: FC = () => {
   return (
-    <>
-      <main className="flex flex-col items-center ">
+    <div className="flex flex-col items-center">
+      <main className="w-full flex flex-col items-center">
         <h1 className=" text-facebook-blue font-klavika text-2xl my-2">
           odinbook
         </h1>
@@ -22,11 +22,53 @@ const SignIn: FC = () => {
             type="password"
             placeholder="Password"
           />
-          <button type="submit">Log In</button>
+          <button
+            className=" bg-facebook-blue text-white mx-3 my-1 rounded font-roboto text-lg p-1  hover:shadow-inner"
+            type="submit"
+          >
+            Log In
+          </button>
         </form>
+        <span className=" my-2 text-facebook-blue text-sm">
+          Forgotten password?
+        </span>
       </main>
-      <footer>Footer</footer>
-    </>
+      <div className="flex w-full items-center">
+        <div className="w-full ml-3 h-px bg-gray-300 box-border"></div>
+        <span className="whitespace-nowrap mx-4 text-gray-800 text-sm">
+          {" "}
+          or{" "}
+        </span>
+        <div className="w-full mr-3 h-px bg-gray-300"></div>
+      </div>
+      <button className=" bg-[#00A400] rounded font-roboto p-1 px-3 my-3 text-white hover:shadow-inner">
+        Create New Account
+      </button>
+      <footer className=" text-stone-500 text-center text-xs w-[75%] ">
+        <span>
+          This is a recreation of{" "}
+          <a
+            className="text-sky-400"
+            href="https://about.facebook.com/meta/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            facebook
+          </a>
+          , created as an assignment for
+          <a
+            className="text-sky-400"
+            href="https://www.theodinproject.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            The Odin Project
+          </a>
+          .
+        </span>
+      </footer>
+    </div>
   );
 };
 
