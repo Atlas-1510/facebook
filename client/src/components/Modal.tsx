ec39365b064f4778646e7b285d9d490a7bbb477b
Jason Aravanis <j.aravanis@icloud.com>
2022-01-28 21:15:04 +1100

    Refactor modal, fix portal use for testing

--- client/src/components/Modal.tsx
+++ client/src/components/Modal.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactNode, MouseEvent } from "react";
+import React, { FC, ReactNode, MouseEvent, useEffect } from "react";
 import ReactDom from "react-dom";
 import { IconContext } from "react-icons";
 import { IoMdClose } from "react-icons/io";
@@ -12,15 +12,31 @@ type Props = {
 };
 
 const Modal: FC<Props> = ({ children, open, onClose, title, subtext }) => {
+  let portal = document.getElementById("portal");
+  if (!portal) {
+    portal = document.createElement("div");
+    portal.setAttribute("id", "portal");
+    document.body.appendChild(portal);
+  }
+
+  useEffect(() => {
+    return () => {
+      if (portal) {
+        portal.remove();
+      }
+    };
+  }, [portal]);
+
   const closeModal = (e: MouseEvent<HTMLElement>): void => {
     e.stopPropagation();
     onClose();
   };
 
   if (!open) {
+    portal.remove();
     return null;
   }
-  const portal: HTMLElement = document.getElementById("portal")!;
+
   return ReactDom.createPortal(
     <div
       className="absolute top-0 left-0 right-0 bottom-0 bg-stone-200/60 z-50"
@@ -28,12 +44,16 @@ const Modal: FC<Props> = ({ children, open, onClose, title, subtext }) => {
     >
       <div className=" w-full h-full grid place-items-center">
         <section
+          role="dialog"
+          aria-labelledby="title"
           className="bg-white rounded-lg w-full max-w-sm md:max-w-md drop-shadow-2xl flex flex-col items-center p-4"
           onClick={(e) => e.stopPropagation()}
         >
           <div className="flex w-full justify-between">
             <div>
-              <h1 className=" text-3xl font-roboto">{title}</h1>
+              <h1 id="title" className=" text-3xl font-roboto">
+                {title}
+              </h1>
               <p className=" text-sm text-zinc-500 font-roboto mt-1">
                 {subtext}
               </p>
