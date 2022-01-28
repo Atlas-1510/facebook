8a8cf79f684475242378965e325b6d243f89d3a9
Jason Aravanis <j.aravanis@icloud.com>
2022-01-28 21:15:39 +1100

    Add onClick prop to primary button component

--- client/src/components/PrimaryButton.tsx
+++ client/src/components/PrimaryButton.tsx
@@ -1,14 +1,16 @@
-import React, { FC } from "react";
+import React, { FC, SyntheticEvent } from "react";
 
 type Props = {
   title: string;
+  onClick?: (e: SyntheticEvent) => Promise<void>;
 };
 
-const PrimaryButton: FC<Props> = ({ title }) => {
+const PrimaryButton: FC<Props> = ({ title, onClick }) => {
   return (
     <button
       className=" bg-facebook-blue hover:bg-blue-600 text-white my-1 rounded font-roboto text-lg p-2  hover:shadow-inner"
       type="submit"
+      onClick={onClick}
     >
       {title}
     </button>
