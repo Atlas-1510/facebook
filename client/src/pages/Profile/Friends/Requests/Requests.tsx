e964ba707223ae6161ff85a224cc5e4e21c084e2
Jason Aravanis <j.aravanis@icloud.com>
2022-02-04 19:33:28 +1100

    Add search filter to Friend Requests page

--- client/src/pages/Profile/Friends/Requests/Requests.tsx
+++ client/src/pages/Profile/Friends/Requests/Requests.tsx
@@ -1,11 +1,14 @@
 import { AuthContext, User } from "../../../../contexts/Auth";
 import FriendTile from "../../../../components/common/FriendTile";
-import { useContext } from "react";
+import { useContext, useEffect, useRef } from "react";
 import axios from "axios";
 import { useQuery } from "react-query";
+import { useOutletContext } from "react-router-dom";
 
 const Requests = () => {
   const { user } = useContext(AuthContext);
+  const searchInput: string = useOutletContext();
+  console.log(searchInput);
 
   const getRequests = async () => {
     const inbound = await (async () => {
@@ -43,25 +46,52 @@ const Requests = () => {
     enabled: !!user?._id,
   });
 
+  const generateTile = (contact: User) => (
+    <FriendTile key={contact._id} contact={contact} />
+  );
+
+  const filterRequests = (arr: User[]) => {
+    return arr.filter((friend: User) =>
+      friend.fullName.toLowerCase().includes(searchInput.toLowerCase())
+    );
+  };
+
+  let filteredInboundRequests = (() => {
+    if (!requests || !requests.inbound) {
+      return [];
+    }
+    if (!searchInput) {
+      return requests.inbound;
+    } else {
+      return filterRequests(requests.inbound);
+    }
+  })();
+
+  let filteredOutboundRequests = (() => {
+    if (!requests || !requests.outbound) {
+      return [];
+    }
+    if (!searchInput) {
+      return requests.outbound;
+    } else {
+      return filterRequests(requests.outbound);
+    }
+  })();
+
   return (
     <section className=" mt-3">
       <div className="flex flex-col my-3">
         <h2 className="text-zinc-800 font-medium text-xl mb-2">Sent</h2>
         <div className=" mt-3 grid grid-cols-2 gap-1">
-          {requests?.outbound &&
-            requests.outbound.map((contact: User) => (
-              <FriendTile key={contact._id} contact={contact} />
-            ))}
+          {filteredOutboundRequests &&
+            filteredOutboundRequests.map(generateTile)}
         </div>
       </div>
 
       <div className="flex flex-col my-3">
         <h2 className="text-zinc-800 font-medium text-xl mb-2">Recieved</h2>
         <div className=" mt-3 grid grid-cols-2 gap-1">
-          {requests?.inbound &&
-            requests.inbound.map((contact: User) => (
-              <FriendTile key={contact._id} contact={contact} />
-            ))}
+          {filteredInboundRequests && filteredInboundRequests.map(generateTile)}
         </div>
       </div>
     </section>
