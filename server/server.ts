0fc113d5cdd32e896aef571db43a426258e000e6
Jason Aravanis <j.aravanis@icloud.com>
2022-03-09 10:23:07 +1100

    Fix server configuration for production environment

--- server/server.ts
+++ server/server.ts
@@ -11,11 +11,9 @@ mongoConfig();
 app.listen(process.env.PORT, () =>
   console.log(`Listening on port ${process.env.PORT}`)
 );
-// if (process.env.NODE_ENV === "production") {
-if (true) {
-  const dir = path.join(__dirname, "../../client/build");
-  console.log(dir);
-  app.use(express.static(dir));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../../client/build")));
 
   app.get("/", (req, res, next) => {
     res.sendFile(path.join(__dirname, "../../client/build/index.html"));
