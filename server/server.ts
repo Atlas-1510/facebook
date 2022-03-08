ce2a77f8a2a3d6f9aab97b25dcdbdc7bad399a12
Jason Aravanis <j.aravanis@icloud.com>
2022-03-08 14:42:54 +1100

    Prepare for heroku deployment

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,20 @@
 import app from "./app/app";
 import mongoConfig from "./mongoConfig";
+import express from "express";
+import path from "path";
+
 const debug = require("debug")("facebook:server");
 require("dotenv").config();
 
 mongoConfig();
-app.listen(process.env.PORT, () =>
-  debug(`Listening on port ${process.env.PORT}`)
-);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res, next) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+} else {
+  app.listen(process.env.PORT, () =>
+    debug(`Listening on port ${process.env.PORT}`)
+  );
+}
